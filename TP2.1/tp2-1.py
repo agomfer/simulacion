4c08382a37481a15b0a220d3742cdee97a35097c
Nicolás Fierro <nico.fierro1@gmail.com>
2021-04-22 16:08:43 -0300

    Generador glc

--- TP2.1/tp2-1.py
+++ TP2.1/tp2-1.py
@@ -1,7 +1,34 @@
-# este sera el archivo para el tp
+# Generador Congruencial Lineal (GCL)
+# Forma --> Xsub(n+1) = (a Xsub(n) + c) mod (m)
 
-# Segunda modificacion. 
+# Están determinados por los parámetros: * Módulo:  m > 0
 
-# Comentario de Nico
+# Multiplicador 0 <= a <= m
+# Incremento c <= m
+# Semilla 0 <= X sub0 <= m
 
-# prueba
+
+valores = [];
+
+def seedLCG(initVal):
+    global rand
+    rand = initVal
+
+def lcg(a, c, m, n):
+    var = []
+    global rand
+    for i in range(n):
+        rand = (a*rand + c) % m
+        var.append(rand)
+        result = '( ' + str(a) + ' * ' + str(rand) + ' + ' + str(c) + ' ) ' + ' % ' + str(m) + ' = ' + str(rand)
+        print(result)
+    return var
+
+
+a = 25214903917
+c = 11
+m = 2**48
+
+seedLCG(2)
+valores=lcg(a,c,m,10)
+
